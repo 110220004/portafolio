b579ce84aedfaeac4d4f83215422252146c6eff8
Wendell Powery <126547506+110220004@users.noreply.github.com>
2024-06-19 08:07:03 -0600

    about with a bit of code
    for later

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => (
+  <div>
+    <h1>About me </h1>
+    <p>Basic description that i haven't added at the moment</p>
+  </div>
+);
+
+export default About;
