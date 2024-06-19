bf01c4cacb42627339281f83c4007639e52a731a
Wendell Powery <126547506+110220004@users.noreply.github.com>
2024-06-19 08:10:52 -0600

    a bit of code for skills
    will update later with with what i am capable of doing

---
+++ src/components/Skills.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Skills = () => (
+  <div>
+    <h1>Abilities</h1>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+    </ul>
+  </div>
+);
+
+export default Skills;
