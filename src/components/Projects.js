188f29cbcc9aea5290850ef0874bfeb6bf841d07
Wendell Powery <126547506+110220004@users.noreply.github.com>
2024-06-19 08:13:05 -0600

    a bit of code for projects
    will add my projects later

---
+++ src/components/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Projects = () => (
+  <div>
+    <h1>Projects</h1>
+    <ul>
+      <li><a href="links have not yest been added">Project 1</a></li>
+    </ul>
+  </div>
+);
+
+export default Projects;
